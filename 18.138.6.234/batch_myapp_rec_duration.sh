0ef04e07440a1c2fa3ebc2cc14853bff1db82206
kpaiboon <kpaiboon@gmail.com>
2020-01-23 11:02:47 +0700

    Add files via upload

---
+++ 18.138.6.234/batch_myapp_rec_duration.sh
@@ -0,0 +1,61 @@
+dirname="/home/ubuntu/gallery"
+shname="myapp_rec_duration.sh"
+fsname="${dirname}/${shname}"
+
+echo ${fsname}
+
+#exit 1 #brake point exit()
+
+ForceWaitSecond=2
+echo "Wait for ${ForceWaitSecond} seconds to start"
+sleep ${ForceWaitSecond}
+
+#ffmpeg
+#Newer versions of ffmpeg don't use 'q' anymore, at least on Ubuntu Oneiric, 
+#instead they say to press Ctrl+C to stop them. 
+#So with a newer version you can simply 
+#use 'killall -INT' to send them SIGINT instead of SIGTERM, and they should exit cleanly.
+
+## signal(SIGINT , sigterm_handler); /* Interrupt (ANSI).    */
+## signal(SIGTERM, sigterm_handler); /* Termination (ANSI).  */
+
+pkill -INT ffmpeg
+sleep 0.3
+pkill -INT ffmpeg
+sleep 0.3
+pkill -INT ffmpeg
+sleep 0.3
+pkill -15 ffmpeg
+killall -15 ${shname}
+sleep 0.3
+
+killall -9 ${shname}
+pkill -15 ffmpeg
+
+
+ForceWaitSecond=3
+echo "Wait for ${ForceWaitSecond} seconds to start backgroud process"
+sleep ${ForceWaitSecond}
+
+
+#exit 1 #brake point exit()
+
+
+${fsname} 0866758044537963 4 &
+${fsname} 0866758044537963 3 &
+${fsname} 0866758044537963 2 &
+${fsname} 0866758044537963 1 &
+
+${fsname} 0861585040494534 4 &
+${fsname} 0861585040494534 3 &
+${fsname} 0861585040494534 2 &
+${fsname} 0861585040494534 1 &
+
+${fsname} 0866758042049359 4 &
+${fsname} 0866758042049359 3 &
+${fsname} 0866758042049359 2 &
+${fsname} 0866758042049359 1 &
+
+
+
+echo "done"
